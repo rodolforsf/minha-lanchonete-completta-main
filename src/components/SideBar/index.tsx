51cc76b4ede7a8fd80c0bedcad48f1fdf43bdeb9
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2024-02-11 19:26:59 -0300

    Atualizando

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-import { Container } from "./styles";
+import { Container } from './styles';
 
 import { ReactComponent as LancheIcon } from '../../assets/burger.svg';
 import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg';
@@ -11,44 +11,46 @@ import { ReactComponent as SobremesaIcon } from '../../assets/ice-cream.svg';
 import menuImg from '../../assets/menu.svg';
 
 
-export function SideBar() {
-  const [menuOpen, setMenuOpen] = useState(false);
+export function Sidebar() {
+  const [menuOpen, setMenuOpen] = useState(false)
 
   const handleToggleMenu = () => {
     setMenuOpen(!menuOpen)
   }
 
-  return <Container isMenuOpen={menuOpen}>
-    <button type='button' onClick={handleToggleMenu}>
-      <img src={menuImg} alt='Abrir e fechar o menu' />
-    </button>
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/'>
-            <LancheIcon />
-            <span>Lanches</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='pizzas'>
-            <PizzaIcon />
-            <span>Pizzas</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='bebidas'>
-            <BebidaIcon />
-            <span>Bebidas</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='sobremesas'>
-            <SobremesaIcon />
-            <span>Sobremesas</span>
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </Container>
+  return (
+    <Container isMenuOpen={menuOpen}>
+        <button type='button' onClick={handleToggleMenu}>
+          <img src={menuImg} alt='Abrir e fechar o menu' />
+        </button>
+        <nav>
+          <ul>
+            <li>
+            <NavLink to='/'>
+              <LancheIcon />
+              <span>Lanches</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='pizzas'>
+              <PizzaIcon />
+              <span>Pizzas</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='bebidas'>
+              <BebidaIcon />
+              <span>Bebidas</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='sobremesas'>
+              <SobremesaIcon />
+              <span>Sobremesas</span>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+ )
 }
