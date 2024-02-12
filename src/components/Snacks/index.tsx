c03d9bffe987107e41317ba9b6b90a0a1179690e
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2024-02-12 12:59:26 -0300

    Criando Loading de dados

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,33 +1,32 @@
-import { FiPlus } from 'react-icons/fi';
-import { Container } from './styles';
-import { currencyFormat } from '../../helpers/currencyFormat';
+import { FiPlus } from 'react-icons/fi'
 
-interface Snack {
-    id: number;
-    name: string;
-    image: string;
-    description: string;
-    price: number;
-  }
-  
-  interface SnacksProps {
-    snacks: Snack[];
-  }
+import { currencyFormat } from '../../helpers/currencyFormat'
+import { SkeletonSnack } from '../SnackTitle/SkeletonSnack'
+
+import { Container } from './styles'
+
+interface SnacksProps {
+  snacks: any[]
+}
 
 export function Snacks({ snacks }: SnacksProps) {
   return (
     <Container>
-      {snacks.map((snack) => (
-        <div key={snack.id} className='snack'>
-          <h2>{snack.name}</h2>
-          <img src={snack.image} alt={snack.name} />
-          <p>{snack.description}</p>
-          <div>
-            <strong>{currencyFormat(snack.price)}</strong>
-            <button type='button'><FiPlus /></button>
-          </div>
-        </div>
-      ))}
+      {!snacks.length
+        ? [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
+        : snacks.map((snack) => (
+            <div key={snack.id} className='snack'>
+              <h2>{snack.name}</h2>
+              <img src={snack.image} alt={snack.name} />
+              <p>{snack.description}</p>
+              <div>
+                <strong>{currencyFormat(snack.price)}</strong>
+                <button type='button'>
+                  <FiPlus />
+                </button>
+              </div>
+            </div>
+          ))}
     </Container>
   )
 }
