4f93436257281174326a38db7abb44e5ba078941
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2024-02-12 10:18:43 -0300

    Carregando os dados via API

--- src/pages/Main/Lanches/index.tsx
+++ src/pages/Main/Lanches/index.tsx
@@ -1,32 +1,26 @@
+import { useState, useEffect } from "react";
 import { Head } from "../../../components/Head";
 import { SnackTitle } from "../../../components/SnackTitle";
 import { Snacks } from "../../../components/Snacks";
 
+import { getLanches } from "../../../services/api";
+
 export default function Lanches() {
-  const data = [
-    {
-      id: 1,
-      snack: 'burguer',
-      name:'Mega',
-      description: 'O artesanal tamanho familia recheado com três carnes suculentas, queijo e bacon.',
-      price: 25.5,
-      image: 'https://i.imgur.com/upjIUnG.jpg',
-    },
-    {
-      id: 2,
-      snack: 'burguer',
-      name:'Extra bacon',
-      description: 'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
-      price: 23.5,
-      image: 'https://i.imgur.com/B4J04AJ.jpg',
-    },
-  ]
+  const [lanches, setLanches] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const lanchesRequest = await getLanches()
+
+      setLanches(lanchesRequest.data)
+    })()
+  }, [])
 
   return (
     <>
       <Head title='Lanches' description='Nossos melhores lanches' />
       <SnackTitle>Lanches</SnackTitle>
-      <Snacks snacks={data}></Snacks>
+      <Snacks snacks={lanches}></Snacks>
     </>
   )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,8 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 })
 
+export const getLanches = () => api.get('/lanches')
+export const getPizzas = () => api.get('/pizzas')
+export const getBebidas = () => api.get('/bebidas')
+export const getSobremesas = () => api.get('/sobremesas')
 export default api
