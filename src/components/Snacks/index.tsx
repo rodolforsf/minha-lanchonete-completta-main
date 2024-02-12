c622a98ace5537a26a1da15c91105e2df548fd05
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2024-02-12 08:40:46 -0300

    Adicionando produtos ao feed

---
+++ src/components/Snacks/index.tsx
@@ -0,0 +1,33 @@
+import { FiPlus } from 'react-icons/fi';
+import { Container } from './styles';
+import { currencyFormat } from '../../helpers/currencyFormat';
+
+interface Snack {
+    id: number;
+    name: string;
+    image: string;
+    description: string;
+    price: number;
+  }
+  
+  interface SnacksProps {
+    snacks: Snack[];
+  }
+
+export function Snacks({ snacks }: SnacksProps) {
+  return (
+    <Container>
+      {snacks.map((snack) => (
+        <div key={snack.id} className='snack'>
+          <h2>{snack.name}</h2>
+          <img src={snack.image} alt={snack.name} />
+          <p>{snack.description}</p>
+          <div>
+            <strong>{currencyFormat(snack.price)}</strong>
+            <button type='button'><FiPlus /></button>
+          </div>
+        </div>
+      ))}
+    </Container>
+  )
+}
--- src/pages/Main/Lanches/index.tsx
+++ src/pages/Main/Lanches/index.tsx
@@ -1,11 +1,32 @@
 import { Head } from "../../../components/Head";
 import { SnackTitle } from "../../../components/SnackTitle";
+import { Snacks } from "../../../components/Snacks";
 
 export default function Lanches() {
+  const data = [
+    {
+      id: 1,
+      snack: 'burguer',
+      name:'Mega',
+      description: 'O artesanal tamanho familia recheado com três carnes suculentas, queijo e bacon.',
+      price: 25.5,
+      image: 'https://i.imgur.com/upjIUnG.jpg',
+    },
+    {
+      id: 2,
+      snack: 'burguer',
+      name:'Extra bacon',
+      description: 'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
+      price: 23.5,
+      image: 'https://i.imgur.com/B4J04AJ.jpg',
+    },
+  ]
+
   return (
     <>
       <Head title='Lanches' description='Nossos melhores lanches' />
       <SnackTitle>Lanches</SnackTitle>
+      <Snacks snacks={data}></Snacks>
     </>
   )
 }
